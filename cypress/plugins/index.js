33e5f9af4fbfc22f1063ce4717036bf760b7c722
singh-sukhpreet <sukhi.singh4424@gmail.com>
2024-08-14 15:28:44 -0600

    changed file size to  300mb

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -46,7 +46,7 @@ module.exports = async (on, config) => {
       if (fs.existsSync(filePath)) {
         return '1GB file already exists at ' + filePath
       }
-      const fileSizeInMB = 100
+      const fileSizeInMB = 300
       const fileSizeInBytes = fileSizeInMB * 1024 * 1024
       const buffer = Buffer.alloc(fileSizeInBytes, '0')
       fs.writeFileSync(filePath, buffer)
