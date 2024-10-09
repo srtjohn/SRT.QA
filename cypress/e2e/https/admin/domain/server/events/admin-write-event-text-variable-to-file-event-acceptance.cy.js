ced888708b1868e53172d3ba2f5334ca04d441a3
singh-sukhpreet <sukhi.singh4424@gmail.com>
2024-10-09 17:12:36 -0600

    created test script for NX-I1264

--- cypress/e2e/https/admin/domain/server/events/admin-write-event-text-variable-to-file-event-acceptance.cy.js
+++ cypress/e2e/https/admin/domain/server/events/admin-write-event-text-variable-to-file-event-acceptance.cy.js
@@ -2,9 +2,9 @@ import label from '../../../../../../fixtures/label.json'
 
 /**
  * @description
- * This spec file contains test to verify that admin can write system date and time variable in custom log or file
+ * This spec file contains test to verify that admin can write event text variable in custom log or file
  *
- * @issueID - NX-I1265
+ * @issueID - NX-I1264
  *
  * @breadcrumb
  * Login > {existing server} > events > create new event
@@ -25,7 +25,7 @@ describe('Login > {existing server} > events > create new event', () => {
     password: adminData.adminPassword
   }
   const createUserDetails = {
-    username: `qa-auto-date-time-event-user-${Cypress.dayjs().format('ssmmhhMMYY')}`,
+    username: `qa-auto-event-text-event-user-${Cypress.dayjs().format('ssmmhhMMYY')}`,
     password: '123456',
     serverName: label.autoServerName
   }
@@ -38,13 +38,14 @@ describe('Login > {existing server} > events > create new event', () => {
 
   const remoteDir = '/'
   const localPath = '../fixtures/local.txt'
-  const remoteDirFile = './file2.txt'
+  const newDir = `/New Folder ${Cypress.dayjs().format('ssmhMMY')}`
+  const remoteDirFile = `.${newDir}/file2.txt`
   const actionType = label.writeToFile
-  const eventName = `file_upload_event_date_time${Cypress.dayjs().format('ssmmhhMMYY')}`
-  const eventDescription = 'this event is used to write system date to a file when a file upload happens'
+  const eventName = `file_upload_event_text${Cypress.dayjs().format('ssmmhhMMYY')}`
+  const eventDescription = 'this event is used to write event text to a file when a file upload happens'
   const customFileName = 'log.txt'
-  const customFilePath = './log.txt'
-  const customText = 'the system date and time is %SYSDATETIME%'
+  const customFilePath = `./${customFileName}`
+  const customText = '%EVENTTEXT%'
 
   beforeEach('login', () => {
     cy.postLoginAuthenticateApiRequest(userInfo).then(($response) => {
@@ -67,27 +68,26 @@ describe('Login > {existing server} > events > create new event', () => {
     // adding action
     cy.createAction(actionType, customText, filePath, eventName, eventDescription)
 
+    cy.task('sftpCreateDirectory', { remoteDir: newDir, configSFTP }).then(p => {
+      expect(`${JSON.stringify(p)}`).to.equal(`"${newDir} directory created"`)
+      cy.task('endSFTPConnection')
+    })
     // uploading a file
     cy.task('sftpUploadFile', { localPath, remoteDirFile, configSFTP }).then(p => {
-      const date = new Date()
-      const formattedDate = `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')} ${String(date.getHours() + 1).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:`
       expect(`${JSON.stringify(p)}`).to.equal(`"Uploaded data stream to ${remoteDirFile}"`)
       cy.task('endSFTPConnection')
-      cy.task('sftpReadFile', { remoteDirFile: customFilePath, configSFTP }).then(p => {
-        console.log(formattedDate)
-        expect(p).to.include(formattedDate)
-        cy.task('endSFTPConnection')
-      })
     })
-
     // verify if log file is created
     cy.task('sftpListFiles', { remoteDir, configSFTP }).then(files => {
       const fileName = files.map(file => file.name)
       expect(fileName).to.include(customFileName)
       cy.task('endSFTPConnection')
     })
-
     // verify the content in the file
+    cy.task('sftpReadFile', { remoteDirFile: customFilePath, configSFTP }).then(p => {
+      expect(p).to.include(label.uploadWriteSuccess)
+      cy.task('endSFTPConnection')
+    })
 
     // deleting the file
     cy.task('sftpDeleteFile', { newRemoteDir: customFilePath, configSFTP }).then(p => {
