f8decb54cce75bcd80e5268adf1903666760a387
singh-sukhpreet <sukhi.singh4424@gmail.com>
2024-11-01 14:16:48 -0600

    completed script for nx1209

--- cypress/e2e/https/admin/domain/server/users/admin-check-file-download-user-inherit-from-group-acceptance.cy.js
+++ cypress/e2e/https/admin/domain/server/users/admin-check-file-download-user-inherit-from-group-acceptance.cy.js
@@ -1,13 +1,14 @@
 import navigationSelectors from '../../../../../../../selectors/navigation/left-navigation-selectors.json'
 import label from '../../../../../../fixtures/label.json'
-import dashboardSelectors from '../../../../../../../selectors/dashboard-selectors.json'
+import userDirSelectors from '../../../../../../../selectors/user-dir-selectors.json'
 import userSelectors from '../../../../../../../selectors/user/user-selectors.json'
+import htmlTagSelectors from '../../../../../../../selectors/htlm-tag-selectors.json'
 
 /**
  * @description
  * This spec file contains test to verify that editing user details in user ui does not affect group membership
  *
- * @IssueID NX-I1394
+ * @IssueID NX-I1209
  *
  * @breadcrumb
  * Login > {existing server} > users > edit
@@ -27,41 +28,65 @@ describe('Login > {existing server} > users > edit', () => {
     username: adminData.adminUsername,
     password: adminData.adminPassword
   }
+  const fileName = 'NewFile-Do-Not-Delete.txt'
   const userDetails = {
     username: `qa-auto-user-${Cypress.dayjs().format('ssmmhhMMYY')}`,
     password: 'testing123',
     serverName: label.autoServerName
   }
-  const groupDetails = {
-    groupName: label.autoGroupName
+
+  function setDirectoryToGroup (username, menuOption) {
+    cy.contains(htmlTagSelectors.div, username).scrollIntoView().parents(userSelectors.parentCell)
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).within(() => {
+        cy.get(htmlTagSelectors.button).click({ force: true })
+      })
+    cy.get(userSelectors.parentUsers).contains(menuOption).click()
+    cy.clickButton(label.next)
+    cy.contains(htmlTagSelectors.div, label.autoGroupName).parents(userSelectors.parentCell)
+      .prev(htmlTagSelectors.div).within(() => {
+        cy.get(htmlTagSelectors.button).click({ force: true })
+      })
+    cy.clickButton(label.next)
   }
-  let userGUID
+
+  function clickOnDropdown (tabLabel) {
+    cy.get(userSelectors.userInfoDialog).within(() => {
+      cy.get(userSelectors.selectGroupOptionsDialog).eq(2).within(() => {
+        cy.get(userDirSelectors.gridItem).contains(tabLabel).next().click()
+      })
+    })
+  }
   beforeEach('login', () => {
     cy.postLoginAuthenticateApiRequest(userInfo).then(($response) => {
       userDetails.bearerToken = $response.Response.SessionInfo.BearerToken
     })
     cy.postCreateUserApiRequest(userDetails).then(($response) => {
       expect($response.Response.Username).to.equal(userDetails.username)
-      userGUID = $response.Response.UserGUID
     })
     cy.login(adminData.adminBaseUrl, userInfo.username, userInfo.password)
   })
-
   it('Verify that during user edit, group membership is not affected', () => {
     cy.get(navigationSelectors.textLabelSelector).contains(label.autoDomainName).click()
     cy.get(navigationSelectors.textLabelSelector).contains(label.autoServerName).should('be.visible').click()
     cy.get(navigationSelectors.textLabelSelector).contains(label.users).should('be.visible').click()
-    cy.editUser(userDetails.username, label.editUserAssignedGroups, true)
-    // login to user url
+    setDirectoryToGroup(userDetails.username, label.editUserAssignedGroups)
+    clickOnDropdown(label.primaryGroup)
+    cy.get(userDirSelectors.buttonList).contains(label.autoGroupName).click()
+    clickOnDropdown(label.homeDir)
+    cy.get(userDirSelectors.buttonList).contains(label.inheritFromGroup).click()
+    cy.clickButton(label.finish)
     cy.login('', userDetails.username, userDetails.password)
-    cy.get(dashboardSelectors.profileIcon).click()
-    cy.get(dashboardSelectors.dashBoardList).contains(label.myProfile).should('be.visible').click()
-    cy.get(userSelectors.usernameField).type(`updated ${userDetails.username}`)
-    cy.get(dashboardSelectors.dashBoardList).contains(label.save).click()
-    // verify if user group membership is not affected
-    cy.getGroupsInfoApiRequest(userDetails, groupDetails).then(($response) => {
-      expect($response.Response.GroupName).to.equal(groupDetails.groupName)
-      expect($response.Response.MemberUsers).to.have.property(userGUID)
+    cy.contains(htmlTagSelectors.div, fileName).parents(userDirSelectors.parentCell)
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).should('exist')
+      .next(htmlTagSelectors.div).click()
+    cy.get(userDirSelectors.editParent).eq(5).within(() => {
+      cy.get(userDirSelectors.bulkDownload).click()
     })
   })
 
